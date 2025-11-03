3dadabc11325447876a18a7e4dc4ba54f9daf4ac
Chandramali Thilakaratne <sahanpradeeptha@gmail.com>
2025-11-03 23:17:16 +0530

    modules added

--- src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+127d760e0276fafe0da95abd9b45bc29ee99590a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
